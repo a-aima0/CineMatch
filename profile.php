2d87bea5edaebe067bb99df89e251a59389984a2
Aima <151074986+a-aima0@users.noreply.github.com>
2025-03-25 15:40:09 +0000

    updated profile page css

--- profile.php
+++ profile.php
@@ -33,7 +33,6 @@ if ($stmt = $conn->prepare($queryUserDetails)) {
     die("Error: sql query failed");
 }
 
-$conn->close();
 ?>
 
 
@@ -44,27 +43,61 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CineMatch - <?php echo htmlspecialchars($user['username']); ?> </title>
-    <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&family=Montserrat:wght@300;400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="profilestyle.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
 </head>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <body>
 
 <?php include_once('header.php'); ?>
 
-    <h2>User Profile</h2>
-    <p><strong>First Name:</strong> <?php echo htmlspecialchars($user['firstName']); ?></p>
-    <p><strong>Last Name:</strong> <?php echo htmlspecialchars($user['lastName']); ?></p>
-    <p><strong>Username:</strong> <?php echo htmlspecialchars($user['username']); ?></p>
-    <p><strong>Email:</strong> <?php echo htmlspecialchars($user['email']); ?></p>
+<div class="page">
+    <div class="profile">
+        <div class="profile-pic-container">
+            <img class="profile-pic" src="assets/profilepic.png" alt="Profile Picture">
+        </div>
+        <div class="profile-info">
+            <p><strong>First Name:</strong> <?php echo htmlspecialchars($user['firstName']); ?></p>
+            <p><strong>Last Name:</strong> <?php echo htmlspecialchars($user['lastName']); ?></p>
+            <p><strong>Username:</strong> <?php echo htmlspecialchars($user['username']); ?></p>
+            <p><strong>Email:</strong> <?php echo htmlspecialchars($user['email']); ?></p>
 
+            <a href="#" class="change-password">Change Password</a>
+            <a href="#" class="delete-account">Delete Account</a>
+        </div>
+        <div class="logout-button" >
+            <a href="logout.php">Logout</a>
+        </div>
+    </div>
+
+<!--    <div class="watchlist-container">-->
+<!--        <h1 class="watchlist-title">Watchlist</h1>-->
+<!--        <div class="watchlist-wrapper">-->
+<!--            <i class="bx bx-chevron-left arrow left-arrow"></i>-->
+<!--            <div class="watchlist">-->
+<!--                <div class="watchlist-item">-->
+<!--                    <img class="watchlist-item-img" src="assets/film2.jpg" alt="">-->
+<!--                </div>-->
+<!--                <div class="watchlist-item">-->
+<!--                    <img class="watchlist-item-img" src="assets/film2.jpg" alt="">-->
+<!--                </div>-->
+<!---->
+<!--            </div>-->
+<!--            <i class="bx bx-chevron-right arrow right-arrow"></i>-->
+<!--        </div>-->
+<!--    </div>-->
+</div>
+
+
+
+<script src="act.js"></script>
 <script src="script.js"></script>
 
-<div class="logout-button"  style="float:left;">
-    <a href="logout.php">Logout</a>
-</div>
+
+
+
+
 
 </body>
 </html>
