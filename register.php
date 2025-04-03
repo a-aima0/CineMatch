b3a345affa15b3994ea1ca7efe3e6d0c6fa79e80
Aima <151074986+a-aima0@users.noreply.github.com>
2025-04-03 10:29:46 +0100

    updated login and registration page css

--- register.php
+++ register.php
@@ -7,45 +7,56 @@ session_start();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang = "en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <title>Register</title>
+    <meta name="viewport" content="width-device-width,
+         initial-scale=1">
+    <title>CineMatch - Registration</title>
+    <link rel="stylesheet" href="regstyle.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-    <h1>Registration</h1>
-    <form action="addUser.php"  method="POST">
-            <label for="firstName">First Name</label>
-            <input type="text" name="firstName" id="firstName" placeholder="First Name" required>
+<div class="container">
+    <div class="leftBox"></div>
+    <div class="wrapper">
+        <form action="addUser.php" method="post">
+            <h1>Welcome to Cinematch</h1>
+            <div class="input-box">
+                <input type="text" placeholder="First name" name="firstName" id="firstName" required>
+            </div>
+            <div class="input-box">
+                <input type="text" placeholder="Last name" name="lastName" id="lastName" required>
+            </div>
+            <div class="input-box">
+                <input type="text" placeholder="Enter Email" name="email" id="email" required>
+                <i class='bx bxs-envelope'></i>
+            </div>
+            <div class="input-box">
+                <input type="text" placeholder="Create Username"  name="username" id="username" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Create Password" name="password" id="password" required>
+                <i class='bx bxs-lock-alt' ></i>
+            </div>
+            <div class="remember">
+                <label><input type="checkbox">Remember me</label>
+            </div>
+            <input type="hidden" name="table" value="users"/>
+            <button type="submit" class="btn"  id="submit">Create account</button>
+        </form>
 
-            <label for="lastName">Last Name</label>
-            <input type="text" name="lastName" id="lastName" placeholder="Last Name" required>
+        <?php
+        if (isset($_SESSION['response'])) {
+            echo '<p style="color:' . ($_SESSION['response']['success'] ? 'green' : 'red') . ';">' .
+                htmlspecialchars($_SESSION['response']['message']) . '</p>';
+            unset($_SESSION['response']);
+        }
+        ?>
 
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Username" required>
-
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Email" required>
-
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Password" required >
-
-        <input type="hidden" name="table" value="users"/>
-        <button type="submit" id="submit">Add User</button>
-    </form>
-
-
-    <?php
-    if (isset($_SESSION['response'])) {
-        echo '<p style="color:' . ($_SESSION['response']['success'] ? 'green' : 'red') . ';">' .
-            htmlspecialchars($_SESSION['response']['message']) . '</p>';
-        unset($_SESSION['response']);
-    }
-    ?>
-
-    <a href="login.php">Login</a>
-
-
+        <a href="login.php">Login</a>
+    </div>
+</div>
 </body>
 </html>
